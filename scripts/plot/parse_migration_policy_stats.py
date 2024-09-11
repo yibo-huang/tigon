3a72fbd5ca13646a7f4e741fb0802f895e71d1bc
ybhuang <ybhuang@cs.utexas.edu>
2024-09-11 03:13:57 +0000

    collect more stats

--- scripts/plot/parse_migration_policy_stats.py
+++ scripts/plot/parse_migration_policy_stats.py
@@ -44,27 +44,32 @@ def construct_input_list_ycsb(res_dir, zipf_theta):
         return input_file_list
 
 def get_row(input):
-        tputs = list()
-        tputs.append(input[0])
-        # Tput
+        row = list()
+        row.append(input[0])
+        # tput
         for line in fileinput.FileInput(input[1]):
                 tokens = line.strip().split()
                 if len(tokens) == 7 and tokens[4] == "total" and tokens[5] == "commit:":
-                        tputs.append(float(tokens[6]))
-        # MsgExchange
+                        row.append(float(tokens[6]))
+
+        # msg_exchange number, size
         for line in fileinput.FileInput(input[1]):
                 tokens = line.strip().split()
-                if len(tokens) > 3 and tokens[3] == "Dispatcher.h:153]":
-                        tputs.append(tokens[9])
-        # local_CXL_access_ratio, remote_access_with_req_ratio, Move_in, Move_out
+                if len(tokens) > 7 and tokens[6] == "type:" and tokens[7] == "4":
+                        row.append(tokens[9])
+                        row.append(tokens[12])
+
+        # abort_rate, local_CXL_access_ratio, remote_access_with_req_ratio, data_move_in, data_move_out
         for line in fileinput.FileInput(input[1]):
                 tokens = line.strip().split()
                 if len(tokens) > 3 and tokens[3] == "Coordinator.h:275]":
-                        tputs.append(tokens[30].split(",")[0].replace("(", "").replace(")", ""))
-                        tputs.append(tokens[35].split(",")[0].replace("(", "").replace(")", ""))
-                        tputs.append(tokens[37].split(",")[0])
-                        tputs.append(tokens[39])
-        return tputs
+                        row.append(tokens[11].replace(",", ""))
+                        row.append(tokens[30].replace(",", "").replace("(", "").replace(")", ""))
+                        row.append(tokens[35].replace(",", "").replace("(", "").replace(")", ""))
+                        row.append(tokens[37].replace(",", ""))
+                        row.append(tokens[39])
+
+        return row
 
 def parse_results(input_list, output_file_name, header_row):
         row = list()
@@ -88,13 +93,13 @@ def parse_results(input_list, output_file_name, header_row):
 def parse_tpcc(res_dir):
         input_file_list = construct_input_list_tpcc(res_dir)
         output_file_name = res_dir + "/tpcc.csv"
-        header_row = ["Policy", "Tput", "MsgExchange", "local_CXL_access_ratio", "remote_access_with_req_ratio", "Move_in", "Move_out"]
+        header_row = ["Policy", "tput", "msg_exchange_num", "msg_exchange_size", "abort_rate", "local_cxl_access_ratio", "remote_access_with_req_ratio", "data_move_in", "data_move_out"]
         parse_results(input_file_list, output_file_name, header_row)
 
 def parse_ycsb(res_dir, zipf_theta):
         input_file_list = construct_input_list_ycsb(res_dir, zipf_theta)
         output_file_name = res_dir + "/ycsb-" + zipf_theta + ".csv"
-        header_row = ["Policy", "Tput", "MsgExchange", "local_CXL_access_ratio", "remote_access_with_req_ratio", "Move_in", "Move_out"]
+        header_row = ["Policy", "tput", "msg_exchange_num", "msg_exchange_size", "abort_rate", "local_cxl_access_ratio", "remote_access_with_req_ratio", "data_move_in", "data_move_out"]
         parse_results(input_file_list, output_file_name, header_row)
 
 
