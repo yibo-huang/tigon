a7493d7c1bdf3321129d43d3741291f9b4836202
ybhuang <ybhuang@cs.utexas.edu>
2024-09-22 22:54:18 +0000

    clang-format BTreeOLC related files

--- common/btree_olc/BTreeOLC.cpp
+++ common/btree_olc/BTreeOLC.cpp
@@ -1,8 +1,9 @@
 #include "BTreeOLC.h"
 
-uint32_t AllocateRWSpinLatchThreadId() {
-    static std::atomic<uint32_t> RWSpinLatchThreadIdAlloc{0};
-    return RWSpinLatchThreadIdAlloc.fetch_add(1) + 1;
+uint32_t AllocateRWSpinLatchThreadId()
+{
+	static std::atomic<uint32_t> RWSpinLatchThreadIdAlloc{ 0 };
+	return RWSpinLatchThreadIdAlloc.fetch_add(1) + 1;
 }
 
 thread_local uint32_t RWSpinLatchThreadId = AllocateRWSpinLatchThreadId();
