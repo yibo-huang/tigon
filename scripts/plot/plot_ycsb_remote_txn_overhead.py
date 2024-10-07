53a31701a2e169ca1fa10ba7f67e985b6391f257
ybhuang <ybhuang@cs.utexas.edu>
2024-10-07 05:15:12 +0000

    update parse and plot scripts and upload new results

--- scripts/plot/plot_ycsb_remote_txn_overhead.py
+++ scripts/plot/plot_ycsb_remote_txn_overhead.py
@@ -37,13 +37,10 @@ x = res_df["Remote_Ratio"]
 
 sundialpasha_cxl_y = res_df["SundialPasha-CXL"]
 sundial_cxl_y = res_df["Sundial-CXL"]
-# lotus_cxl_y = res_df["Lotus-CXL"]
-sundialpasha_net_y = res_df["SundialPasha-NET"]
 sundial_net_y = res_df["Sundial-NET"]
-# lotus_net_y = res_df["Lotus-NET"]
+twopl_cxl_y = res_df["TwoPL-CXL"]
+twopl_net_y = res_df["TwoPL-NET"]
 
-
-# plt.title("TPCC Remote Transaction Overhead", **basic_font)
 plt.xlabel("Multi-host Transaction Percentage", **basic_font)
 plt.ylabel("Throughput (txns/sec)", **basic_font)
 
@@ -51,8 +48,9 @@ plt.ylabel("Throughput (txns/sec)", **basic_font)
 tmp_list = list()
 tmp_list.extend(sundialpasha_cxl_y)
 tmp_list.extend(sundial_cxl_y)
-tmp_list.extend(sundialpasha_net_y)
 tmp_list.extend(sundial_net_y)
+tmp_list.extend(twopl_cxl_y)
+tmp_list.extend(twopl_net_y)
 max_y = max(tmp_list)
 max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
 plt.ylim(0, max_y_rounded_up)
@@ -64,10 +62,9 @@ ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.forma
 # Create the line plot
 plt.plot(x, sundialpasha_cxl_y, color="#ed7d31", marker="o", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="SundialPasha-CXL")
 plt.plot(x, sundial_cxl_y, color="#ffc003", marker="^", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Sundial-CXL")
-# plt.plot(x, lotus_cxl_y, color="#62615d", marker=">", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Lotus-CXL")
-plt.plot(x, sundialpasha_net_y, color="#4372c4", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="SundialPasha-NET")
+plt.plot(x, twopl_cxl_y, color="#62615d", marker=">", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-CXL")
 plt.plot(x, sundial_net_y, color="#CD5C5C", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Sundial-NET")
-# plt.plot(x, lotus_net_y, color="#483D8B", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Lotus-NET")
+plt.plot(x, twopl_net_y, color="#4372c4", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-NET")
 
 # Configure legend
 ax.legend(loc='upper center', frameon=False, fancybox=False, framealpha=1, ncol=2, prop={**basic_font})
