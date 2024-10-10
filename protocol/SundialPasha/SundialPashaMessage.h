6d4e06be49b1b47f1da2dd63fd606908dee926cf
ybhuang <ybhuang@cs.utexas.edu>
2024-10-10 19:56:11 +0000

    SundialPasha: minor update

--- protocol/SundialPasha/SundialPashaMessage.h
+++ protocol/SundialPasha/SundialPashaMessage.h
@@ -33,7 +33,7 @@ class SundialPashaMessageFactory {
 	static std::size_t new_data_migration_message(Message &message, ITable &table, const void *key, uint64_t transaction_id, uint32_t key_offset)
 	{
 		/*
-		 * The structure of a read request: (primary key, read key offset)
+		 * The structure of a data migration request: (primary key, transaction_id, key_offset)
 		 */
 
 		auto key_size = table.key_size();
@@ -70,26 +70,7 @@ class SundialPashaMessageFactory {
 
         static std::size_t new_replication_message(Message &message, ITable &table, const void *key, const void *value, uint64_t commit_ts, bool sync_redo)
 	{
-		/*
-		 * The structure of a replication request: (primary key, field value,
-		 * commit_ts)
-		 */
-
-		auto key_size = table.key_size();
-		auto field_size = table.field_size();
-
-		auto message_size = MessagePiece::get_header_size() + key_size + field_size + sizeof(commit_ts) + sizeof(bool);
-		auto message_piece_header = MessagePiece::construct_message_piece_header(static_cast<uint32_t>(SundialPashaMessage::REPLICATION_REQUEST),
-											 message_size, table.tableID(), table.partitionID());
-
-		Encoder encoder(message.data);
-		encoder << message_piece_header;
-		encoder.write_n_bytes(key, key_size);
-		table.serialize_value(encoder, value);
-		encoder << commit_ts << sync_redo;
-		message.flush();
-		message.set_gen_time(Time::now());
-		return message_size;
+		CHECK(0);
 	}
 };
 
@@ -108,14 +89,13 @@ class SundialPashaMessageHandler {
 		auto value_size = table.value_size();
 
 		/*
-		 * The structure of a read request: (primary key, read key offset)
-		 * The structure of a read response: (value, rts, wts, read key offset)
+		 * The structure of a data migration request: (key, transaction_id, key_offset)
+		 * The structure of a data migration response: (success, key_offset)
 		 */
 
 		auto stringPiece = inputPiece.toStringPiece();
 		uint32_t key_offset;
 		uint64_t transaction_id;
-		uint64_t rts, wts;
                 bool success = false;
 
 		DCHECK(inputPiece.get_message_length() ==
@@ -162,8 +142,8 @@ class SundialPashaMessageHandler {
 		auto value_size = table.value_size();
 
 		/*
-		 * The structure of a read request: (primary key, transaction_id, write-lock key offset in the write-set)
-		 * The structure of a read response: (success, rts, wts, write-lock key offset in the write-set)
+		 * The structure of a data migration request: (key, transaction_id, key_offset)
+		 * The structure of a data migration response: (success, key_offset)
 		 */
 
 		auto stringPiece = inputPiece.toStringPiece();
@@ -201,10 +181,12 @@ class SundialPashaMessageHandler {
                                 txn->abort_lock = true;
                         }
                 }
+
                 // mark it as reference counted so that we know if we need to release it upon commit/abort
                 readKey.set_reference_counted();
 
                 txn->pendingResponses--;
+                txn->network_size += inputPiece.get_message_length();
 	}
 
         static void data_move_out_hint_handler(MessagePiece inputPiece, Message &responseMessage, ITable &table, Transaction *txn)
@@ -214,84 +196,12 @@ class SundialPashaMessageHandler {
 
         static void replication_request_handler(MessagePiece inputPiece, Message &responseMessage, ITable &table, Transaction *txn)
 	{
-		DCHECK(inputPiece.get_message_type() == static_cast<uint32_t>(SundialPashaMessage::REPLICATION_REQUEST));
-		auto table_id = inputPiece.get_table_id();
-		auto partition_id = inputPiece.get_partition_id();
-		DCHECK(table_id == table.tableID());
-		DCHECK(partition_id == table.partitionID());
-		auto key_size = table.key_size();
-		auto field_size = table.field_size();
-
-		/*
-		 * The structure of a replication request: (primary key, field value,
-		 * commit_tid, sync_redo).
-		 * The structure of a replication response: null
-		 */
-
-		DCHECK(inputPiece.get_message_length() == MessagePiece::get_header_size() + key_size + field_size + sizeof(uint64_t) + sizeof(bool));
-
-		auto stringPiece = inputPiece.toStringPiece();
-
-		const void *key = stringPiece.data();
-		stringPiece.remove_prefix(key_size);
-		// auto valueStringPiece = stringPiece;
-		// stringPiece.remove_prefix(field_size);
-
-		uint64_t commit_ts;
-		bool sync_redo = false;
-		Decoder dec(stringPiece);
-		dec >> commit_ts >> sync_redo;
-
-		DCHECK(dec.size() == 0);
-
-		// auto row = table.search(key);
-		// SundialPashaHelper::replica_update(row, valueStringPiece.data(), field_size, commit_ts);
-
-		// std::atomic<uint64_t> &tid = *table.search_metadata(key);
-
-		// uint64_t last_tid = SundialPashaHelper::lock(tid);
-		// DCHECK(last_tid < commit_tid);
-		// table.deserialize_value(key, valueStringPiece);
-		// SundialPashaHelper::unlock(tid, commit_tid);
-
-		// uint64_t lsn = 0;
-		//  if (txn->get_logger()) {
-		//    std::ostringstream ss;
-		//    ss << commit_tid << std::string((const char *)key, key_size) << std::string(valueStringPiece.data(), field_size);
-		//    auto output = ss.str();
-		//    txn->get_logger()->write(output.c_str(), output.size(), sync_redo);
-		//  }
-
-		// if (txn->get_logger() && sync_redo) {
-		//   txn->get_logger()->sync(lsn, [&](){ txn->remote_request_handler(); });
-		// }
-
-		// prepare response message header
-		auto message_size = MessagePiece::get_header_size();
-		auto message_piece_header = MessagePiece::construct_message_piece_header(static_cast<uint32_t>(SundialPashaMessage::REPLICATION_RESPONSE),
-											 message_size, table_id, partition_id);
-
-		star::Encoder encoder(responseMessage.data);
-		encoder << message_piece_header;
-		responseMessage.flush();
-		responseMessage.set_gen_time(Time::now());
+                CHECK(0);
 	}
 
 	static void replication_response_handler(MessagePiece inputPiece, Message &responseMessage, ITable &table, Transaction *txn)
 	{
-		DCHECK(inputPiece.get_message_type() == static_cast<uint32_t>(SundialPashaMessage::REPLICATION_RESPONSE));
-		auto table_id = inputPiece.get_table_id();
-		auto partition_id = inputPiece.get_partition_id();
-		DCHECK(table_id == table.tableID());
-		DCHECK(partition_id == table.partitionID());
-		auto key_size = table.key_size();
-
-		/*
-		 * The structure of a replication response: ()
-		 */
-
-		txn->pendingResponses--;
-		txn->network_size += inputPiece.get_message_length();
+		CHECK(0);
 	}
 
 
