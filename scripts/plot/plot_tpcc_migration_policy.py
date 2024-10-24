0acd441970d16ec79c0f18fd34e2651aa1fec852
ybhuang <ybhuang@cs.utexas.edu>
2024-10-24 02:09:50 +0000

    add scripts to plot migration policy comparison

---
+++ scripts/plot/plot_tpcc_migration_policy.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+if len(sys.argv) != 2:
+        print("Usage: ./plot_tpcc_migration_policy.py res_dir")
+        sys.exit(-1)
+
+res_dir = sys.argv[1]
+
+marker_size = 10.0
+marker_edge_width=1.6
+linewidth = 1.0
+
+###  Common configurations for TPCC and YCSB ###
+basic_font = {'family' : 'times new roman', 'size' : 12}
+plt.xticks(**basic_font)
+plt.yticks(**basic_font)
+
+# Configure grid
+plt.grid(axis='y')
+
+### plot TPCC ###
+res_csv = res_dir + "/tpcc-migration-policy.csv"
+
+# Read the CSV file into a Pandas DataFrame
+res_df = pd.read_csv(res_csv)
+
+# Extract the data
+x = res_df["Remote_Ratio"]
+
+sundialpasha_cxl_nomoveout_y = res_df["SundialPasha-CXL-NoMoveOut"]
+sundialpasha_cxl_fifo_y = res_df["SundialPasha-CXL-FIFO"]
+
+twoplpasha_cxl_nomoveout_y = res_df["TwoPLPasha-CXL-NoMoveOut"]
+twoplpasha_cxl_fifo_y = res_df["TwoPLPasha-CXL-FIFO"]
+
+plt.xlabel("Multi-host Transaction Percentage (NewOrder/Payment)", **basic_font)
+plt.ylabel("Throughput (txns/sec)", **basic_font)
+
+# Configure axis range
+tmp_list = list()
+tmp_list.extend(sundialpasha_cxl_nomoveout_y)
+tmp_list.extend(sundialpasha_cxl_fifo_y)
+tmp_list.extend(twoplpasha_cxl_nomoveout_y)
+tmp_list.extend(twoplpasha_cxl_fifo_y)
+
+max_y = max(tmp_list)
+max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
+plt.ylim(0, max_y_rounded_up)
+
+# Transform Y axises
+ax = plt.subplot(111)
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K' if x != 0 else 0))
+
+# Create the line plot
+plt.plot(x, sundialpasha_cxl_nomoveout_y, color="#ed7d31", marker="o", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="SundialPasha-CXL-HCC")
+plt.plot(x, sundialpasha_cxl_fifo_y, color="#000000", marker="o", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="SundialPasha-CXL-WT")
+plt.plot(x, twoplpasha_cxl_nomoveout_y, color="#ffc003", marker="^", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="SundialPasha-CXL-NT")
+plt.plot(x, twoplpasha_cxl_fifo_y, color="#62615d", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPLPasha-CXL-HCC")
+
+# Configure legend
+ax.legend(frameon=False, fancybox=False, framealpha=1, ncol=2, prop={**basic_font})
+
+plt.savefig(res_dir + "tpcc-migration-policy.pdf", format="pdf", bbox_inches="tight")
