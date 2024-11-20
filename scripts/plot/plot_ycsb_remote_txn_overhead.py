33f3dfabc52629acf99dd77ad594103e1d546b6f
ybhuang <ybhuang@cs.utexas.edu>
2024-11-20 15:49:11 +0000

    remote-txn-overhead updated with Motor

--- scripts/plot/plot_ycsb_remote_txn_overhead.py
+++ scripts/plot/plot_ycsb_remote_txn_overhead.py
@@ -40,6 +40,7 @@ sundial_cxl_y = res_df["Sundial-CXL"]
 sundial_net_y = res_df["Sundial-NET"]
 twopl_cxl_y = res_df["TwoPL-CXL"]
 twopl_net_y = res_df["TwoPL-NET"]
+motor_y = res_df["Motor-RDMA(28thd)"]
 
 plt.xlabel("Multi-host Transaction Percentage", **basic_font)
 plt.ylabel("Throughput (txns/sec)", **basic_font)
@@ -51,6 +52,7 @@ tmp_list.extend(sundial_cxl_y)
 tmp_list.extend(sundial_net_y)
 tmp_list.extend(twopl_cxl_y)
 tmp_list.extend(twopl_net_y)
+tmp_list.extend(motor_y)
 max_y = max(tmp_list)
 max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
 plt.ylim(0, max_y_rounded_up)
@@ -65,6 +67,7 @@ plt.plot(x, sundial_cxl_y, color="#ffc003", marker="^", markersize=marker_size, 
 plt.plot(x, twopl_cxl_y, color="#62615d", marker=">", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-CXL")
 plt.plot(x, sundial_net_y, color="#CD5C5C", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Sundial-NET")
 plt.plot(x, twopl_net_y, color="#4372c4", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-NET")
+plt.plot(x, motor_y, color="#3CB371", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Motor")
 
 # Configure legend
 ax.legend(loc='upper center', frameon=False, fancybox=False, framealpha=1, ncol=2, prop={**basic_font})
