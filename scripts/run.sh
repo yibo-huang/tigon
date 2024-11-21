810c8bd32a44769e05d85a23803697f6196b912f
ybhuang <ybhuang@cs.utexas.edu>
2024-11-21 22:52:45 +0000

    use one partition by default

--- scripts/run.sh
+++ scripts/run.sh
@@ -115,7 +115,7 @@ function run_exp_tpcc {
         typeset WAL_GROUP_COMMIT_BATCH_SIZE=${21}
         typeset GATHER_OUTPUT=${22}
 
-        typeset PARTITION_NUM=$(expr $HOST_NUM \* $WORKER_NUM)
+        typeset PARTITION_NUM=$(expr $HOST_NUM \* 1)  # one partition per host
         typeset SERVER_STRING=$(print_server_string $HOST_NUM)
         typeset i=0
 
@@ -264,7 +264,7 @@ function run_exp_ycsb {
         typeset WAL_GROUP_COMMIT_BATCH_SIZE=${23}
         typeset GATHER_OUTPUT=${24}
 
-        typeset PARTITION_NUM=$(expr $HOST_NUM \* $WORKER_NUM)
+        typeset PARTITION_NUM=$(expr $HOST_NUM \* 1)    # one partition per host
         typeset SERVER_STRING=$(print_server_string $HOST_NUM)
         typeset i=0
 
