999d3d365fb1e34f5aa9f8a8e2d58e3ea4c18390
ybhuang <ybhuang@cs.utexas.edu>
2024-11-23 20:02:13 +0000

    initialize the secondary subscriber table and the access_info table in TATP

--- benchmark/tatp/Database.h
+++ benchmark/tatp/Database.h
@@ -45,7 +45,7 @@ class Database {
 
         std::size_t get_table_num_per_partition()
         {
-                return 2;
+                return 3;
         }
 
 	ITable *find_table(std::size_t table_id, std::size_t partition_id)
@@ -100,28 +100,64 @@ class Database {
 
 		for (auto partitionID = 0u; partitionID < partitionNum; partitionID++) {
                         // subscriber table
-			auto savingsTableID = tatp::subscriber::tableID;
+			auto subscriberTableID = tatp::subscriber::tableID;
 			if (context.protocol == "Sundial") {
 				tbl_subscriber_vec.push_back(
-					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncSundial> >(savingsTableID, partitionID));
+					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncSundial> >(subscriberTableID, partitionID));
                         } else if (context.protocol == "SundialPasha") {
                                 tbl_subscriber_vec.push_back(
-					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncSundialPasha> >(savingsTableID, partitionID));
+					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncSundialPasha> >(subscriberTableID, partitionID));
                         } else if (context.protocol == "TwoPL") {
                                 tbl_subscriber_vec.push_back(
-					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncTwoPL> >(savingsTableID, partitionID));
+					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncTwoPL> >(subscriberTableID, partitionID));
                         } else if (context.protocol == "TwoPLPasha") {
                                 tbl_subscriber_vec.push_back(
-					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncTwoPLPasha> >(savingsTableID, partitionID));
+					std::make_unique<TableHashMap<997, tatp::subscriber::key, tatp::subscriber::value, tatp::subscriber::KeyComparator, tatp::subscriber::ValueComparator, MetaInitFuncTwoPLPasha> >(subscriberTableID, partitionID));
 			} else if (context.protocol != "HStore") {
 				CHECK(0);
 			} else {
 				CHECK(0);
 			}
 
-                        // second subscriber table?
+                        // secondary subscriber table
+                        auto sec_subscriberTableID = tatp::sec_subscriber::tableID;
+			if (context.protocol == "Sundial") {
+				tbl_sec_subscriber_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::sec_subscriber::key, tatp::sec_subscriber::value, tatp::sec_subscriber::KeyComparator, tatp::sec_subscriber::ValueComparator, MetaInitFuncSundial> >(sec_subscriberTableID, partitionID));
+                        } else if (context.protocol == "SundialPasha") {
+                                tbl_sec_subscriber_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::sec_subscriber::key, tatp::sec_subscriber::value, tatp::sec_subscriber::KeyComparator, tatp::sec_subscriber::ValueComparator, MetaInitFuncSundialPasha> >(sec_subscriberTableID, partitionID));
+                        } else if (context.protocol == "TwoPL") {
+                                tbl_sec_subscriber_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::sec_subscriber::key, tatp::sec_subscriber::value, tatp::sec_subscriber::KeyComparator, tatp::sec_subscriber::ValueComparator, MetaInitFuncTwoPL> >(sec_subscriberTableID, partitionID));
+                        } else if (context.protocol == "TwoPLPasha") {
+                                tbl_sec_subscriber_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::sec_subscriber::key, tatp::sec_subscriber::value, tatp::sec_subscriber::KeyComparator, tatp::sec_subscriber::ValueComparator, MetaInitFuncTwoPLPasha> >(sec_subscriberTableID, partitionID));
+			} else if (context.protocol != "HStore") {
+				CHECK(0);
+			} else {
+				CHECK(0);
+			}
 
                         // access_info table
+                        auto accessInfoTableID = tatp::access_info::tableID;
+			if (context.protocol == "Sundial") {
+				tbl_access_info_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::access_info::key, tatp::access_info::value, tatp::access_info::KeyComparator, tatp::access_info::ValueComparator, MetaInitFuncSundial> >(accessInfoTableID, partitionID));
+                        } else if (context.protocol == "SundialPasha") {
+                                tbl_access_info_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::access_info::key, tatp::access_info::value, tatp::access_info::KeyComparator, tatp::access_info::ValueComparator, MetaInitFuncSundialPasha> >(accessInfoTableID, partitionID));
+                        } else if (context.protocol == "TwoPL") {
+                                tbl_access_info_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::access_info::key, tatp::access_info::value, tatp::access_info::KeyComparator, tatp::access_info::ValueComparator, MetaInitFuncTwoPL> >(accessInfoTableID, partitionID));
+                        } else if (context.protocol == "TwoPLPasha") {
+                                tbl_access_info_vec.push_back(
+					std::make_unique<TableHashMap<997, tatp::access_info::key, tatp::access_info::value, tatp::access_info::KeyComparator, tatp::access_info::ValueComparator, MetaInitFuncTwoPLPasha> >(accessInfoTableID, partitionID));
+			} else if (context.protocol != "HStore") {
+				CHECK(0);
+			} else {
+				CHECK(0);
+			}
 
                         // special_facility table
 
@@ -129,14 +165,18 @@ class Database {
 		}
 
 		// there are 5 tables in TATP
-		tbl_vecs.resize(1);
+		tbl_vecs.resize(get_table_num_per_partition());
 
 		auto tFunc = [](std::unique_ptr<ITable> &table) { return table.get(); };
 
 		std::transform(tbl_subscriber_vec.begin(), tbl_subscriber_vec.end(), std::back_inserter(tbl_vecs[0]), tFunc);
+                std::transform(tbl_sec_subscriber_vec.begin(), tbl_sec_subscriber_vec.end(), std::back_inserter(tbl_vecs[1]), tFunc);
+                std::transform(tbl_access_info_vec.begin(), tbl_access_info_vec.end(), std::back_inserter(tbl_vecs[2]), tFunc);
 
 		using std::placeholders::_1;
 		initTables("subscriber", [&context, this](std::size_t partitionID) { subscriberInit(context, partitionID); }, partitionNum, threadsNum, partitioner.get());
+                initTables("sec_subscriber", [&context, this](std::size_t partitionID) { secSubscriberInit(context, partitionID); }, partitionNum, threadsNum, partitioner.get());
+                initTables("access_info", [&context, this](std::size_t partitionID) { accessInfoInit(context, partitionID); }, partitionNum, threadsNum, partitioner.get());
 	}
 
 	void apply_operation(const Operation &operation)
@@ -180,6 +220,7 @@ class Database {
                         boost::interprocess::offset_ptr<void> *cxl_table_ptrs = reinterpret_cast<boost::interprocess::offset_ptr<void> *>(cxl_memory.cxlalloc_malloc_wrapper(
                                         sizeof(boost::interprocess::offset_ptr<void>) * total_table_num, CXLMemory::INDEX_ALLOCATION));
 
+                        // subscriber
                         auto subscriberTableID = subscriber::tableID;
                         cxl_tbl_vecs[subscriberTableID].resize(partitionNum);
                         auto subscriber_cxl_hashtables = reinterpret_cast<CCHashTable *>(cxl_memory.cxlalloc_malloc_wrapper(
@@ -191,6 +232,30 @@ class Database {
                                 cxl_tbl_vecs[subscriberTableID][i] = new CXLTableHashMap<subscriber::key>(cxl_table, subscriberTableID, i);
                         }
 
+                        // secondary subscriber
+                        auto secSubscriberTableID = sec_subscriber::tableID;
+                        cxl_tbl_vecs[secSubscriberTableID].resize(partitionNum);
+                        auto sec_subscriber_cxl_hashtables = reinterpret_cast<CCHashTable *>(cxl_memory.cxlalloc_malloc_wrapper(
+                                        sizeof(CCHashTable) * partitionNum, CXLMemory::INDEX_ALLOCATION));
+                        for (int i = 0; i < partitionNum; i++) {
+                                auto cxl_table = &sec_subscriber_cxl_hashtables[i];
+                                new(cxl_table) CCHashTable(cxl_hashtable_bkt_cnt);
+                                cxl_table_ptrs[secSubscriberTableID * partitionNum + i] = reinterpret_cast<void *>(cxl_table);
+                                cxl_tbl_vecs[secSubscriberTableID][i] = new CXLTableHashMap<sec_subscriber::key>(cxl_table, secSubscriberTableID, i);
+                        }
+
+                        // access_info
+                        auto accessInfoTableID = access_info::tableID;
+                        cxl_tbl_vecs[accessInfoTableID].resize(partitionNum);
+                        auto access_info_cxl_hashtables = reinterpret_cast<CCHashTable *>(cxl_memory.cxlalloc_malloc_wrapper(
+                                        sizeof(CCHashTable) * partitionNum, CXLMemory::INDEX_ALLOCATION));
+                        for (int i = 0; i < partitionNum; i++) {
+                                auto cxl_table = &access_info_cxl_hashtables[i];
+                                new(cxl_table) CCHashTable(cxl_hashtable_bkt_cnt);
+                                cxl_table_ptrs[accessInfoTableID * partitionNum + i] = reinterpret_cast<void *>(cxl_table);
+                                cxl_tbl_vecs[accessInfoTableID][i] = new CXLTableHashMap<access_info::key>(cxl_table, accessInfoTableID, i);
+                        }
+
                         CXLMemory::commit_shared_data_initialization(CXLMemory::cxl_data_migration_root_index, cxl_table_ptrs);
                         LOG(INFO) << "TATP initializes data migration metadata";
                 } else {
@@ -199,11 +264,28 @@ class Database {
                         CXLMemory::wait_and_retrieve_cxl_shared_data(CXLMemory::cxl_data_migration_root_index, &tmp);
                         boost::interprocess::offset_ptr<void> *cxl_table_ptrs = reinterpret_cast<boost::interprocess::offset_ptr<void> *>(tmp);
 
+                        // subscriber
                         auto subscriberTableID = subscriber::tableID;
                         cxl_tbl_vecs[subscriberTableID].resize(partitionNum);
                         for (int i = 0; i < partitionNum; i++) {
                                 auto cxl_table = reinterpret_cast<CCHashTable *>(cxl_table_ptrs[subscriberTableID * partitionNum + i].get());
                                 cxl_tbl_vecs[subscriberTableID][i] = new CXLTableHashMap<subscriber::key>(cxl_table, subscriberTableID, i);
+                        }
+
+                        // secondary subscriber
+                        auto secSubscriberTableID = sec_subscriber::tableID;
+                        cxl_tbl_vecs[secSubscriberTableID].resize(partitionNum);
+                        for (int i = 0; i < partitionNum; i++) {
+                                auto cxl_table = reinterpret_cast<CCHashTable *>(cxl_table_ptrs[secSubscriberTableID * partitionNum + i].get());
+                                cxl_tbl_vecs[secSubscriberTableID][i] = new CXLTableHashMap<sec_subscriber::key>(cxl_table, secSubscriberTableID, i);
+                        }
+
+                        // access_info
+                        auto accessInfoTableID = access_info::tableID;
+                        cxl_tbl_vecs[accessInfoTableID].resize(partitionNum);
+                        for (int i = 0; i < partitionNum; i++) {
+                                auto cxl_table = reinterpret_cast<CCHashTable *>(cxl_table_ptrs[accessInfoTableID * partitionNum + i].get());
+                                cxl_tbl_vecs[accessInfoTableID][i] = new CXLTableHashMap<access_info::key>(cxl_table, accessInfoTableID, i);
                         }
                         LOG(INFO) << "TATP retrieves data migration metadata";
                 }
@@ -265,6 +347,78 @@ class Database {
                 CHECK(success == true);
 	}
 
+        void secSubscriberInit(const Context &context, std::size_t partitionID)
+	{
+		Random random;
+		ITable *table = tbl_sec_subscriber_vec[partitionID].get();
+
+		std::size_t numSubScriberPerPartition = context.numSubScriberPerPartition;
+		std::size_t partitionNum = context.partition_num;
+
+                for (auto i = partitionID * numSubScriberPerPartition; i < (partitionID + 1) * numSubScriberPerPartition; i++) {
+                        DCHECK(context.getPartitionID(i) == partitionID);
+
+                        sec_subscriber::key key;
+
+                        std::stringstream sub_nbr;
+                        sub_nbr << std::setw(15) << std::setfill('0') << i;
+                        key.SUB_NBR.assign(sub_nbr.str());
+
+                        sec_subscriber::value value;
+                        value.S_ID = i;
+
+                        bool success = table->insert(&key, &value);
+                        CHECK(success == true);
+                }
+
+                // secondary subscriber does not need a max key
+                // // insert a max key that represents the upper bound (for next-key locking)
+                // sec_subscriber::key max_key;
+
+                // std::stringstream max_sub_nbr;
+                // max_sub_nbr << std::setw(15) << std::setfill('0') << UINT32_MAX;
+                // max_key.SUB_NBR.assign(max_sub_nbr.str());
+
+                // sec_subscriber::value dummy_value;
+                // bool success = table->insert(&max_key, &dummy_value);
+                // CHECK(success == true);
+	}
+
+        void accessInfoInit(const Context &context, std::size_t partitionID)
+	{
+		Random random;
+		ITable *table = tbl_access_info_vec[partitionID].get();
+
+		std::size_t numSubScriberPerPartition = context.numSubScriberPerPartition;
+		std::size_t partitionNum = context.partition_num;
+
+                for (auto i = partitionID * numSubScriberPerPartition; i < (partitionID + 1) * numSubScriberPerPartition; i++) {
+                        DCHECK(context.getPartitionID(i) == partitionID);
+
+                        uint64_t record_num = random.uniform_dist(1, 4);
+                        for (auto j = 0; j < record_num; j++) {
+                                access_info::key key;
+                                key.S_ID = i;
+                                key.AI_TYPE = random.uniform_dist(1, 4);
+
+                                access_info::value value;
+                                value.DATA_1 = random.uniform_dist(0, 255);
+                                value.DATA_2 = random.uniform_dist(0, 255);
+                                value.DATA_3.assign(random.a_string(ACCESS_INFO_DATA_3_SIZE, ACCESS_INFO_DATA_3_SIZE));
+                                value.DATA_4.assign(random.a_string(ACCESS_INFO_DATA_4_SIZE, ACCESS_INFO_DATA_4_SIZE));
+
+                                bool success = table->insert(&key, &value);
+                                CHECK(success == true);
+                        }
+                }
+
+                // insert a max key that represents the upper bound (for next-key locking)
+                access_info::key max_key(UINT32_MAX, UINT8_MAX);
+                access_info::value dummy_value;
+                bool success = table->insert(&max_key, &dummy_value);
+                CHECK(success == true);
+	}
+
     public:
         // for correctness test
         std::atomic<uint64_t> global_total_commit{ 0 };
@@ -277,6 +431,8 @@ class Database {
 
 	std::vector<std::vector<ITable *> > tbl_vecs;
 	std::vector<std::unique_ptr<ITable> > tbl_subscriber_vec;
+	std::vector<std::unique_ptr<ITable> > tbl_sec_subscriber_vec;
+        std::vector<std::unique_ptr<ITable> > tbl_access_info_vec;
 
         std::vector<std::vector<CXLTableBase *> > cxl_tbl_vecs;
 };
--- benchmark/tatp/Schema.h
+++ benchmark/tatp/Schema.h
@@ -21,12 +21,13 @@ static constexpr auto __BASE_COUNTER__ = __COUNTER__ + 1;
 #undef NAMESPACE_FIELDS
 #define NAMESPACE_FIELDS(x) x(star) x(tatp)
 
+#define SUBSCRIBER_SUB_NBR 15
 #define SUBSCRIBER_BITS_SIZE 2
 #define SUBSCRIBER_HEXES_SIZE 10
 #define SUBSCRIBER_BYTES_SIZE 10
 
 #define SUBSCRIBER_KEY_FIELDS(x, y) x(uint32_t, S_ID)
-#define SUBSCRIBER_VALUE_FIELDS(x, y) x(FixedString<15>, SUB_NBR) y(FixedString<2>, BITS) y(FixedString<10>, HEXES) y(FixedString<10>, BYTES) y(uint32_t, MSC_LOCATION) y(uint32_t, VLR_LOCATION)
+#define SUBSCRIBER_VALUE_FIELDS(x, y) x(FixedString<SUBSCRIBER_SUB_NBR>, SUB_NBR) y(FixedString<SUBSCRIBER_BITS_SIZE>, BITS) y(FixedString<SUBSCRIBER_HEXES_SIZE>, HEXES) y(FixedString<SUBSCRIBER_BYTES_SIZE>, BYTES) y(uint32_t, MSC_LOCATION) y(uint32_t, VLR_LOCATION)
 
 #define SUBSCRIBER_GET_PLAIN_KEY_FUNC           \
         uint64_t get_plain_key() const          \
@@ -35,6 +36,31 @@ static constexpr auto __BASE_COUNTER__ = __COUNTER__ + 1;
         }
 
 DO_STRUCT(subscriber, SUBSCRIBER_KEY_FIELDS, SUBSCRIBER_VALUE_FIELDS, NAMESPACE_FIELDS, SUBSCRIBER_GET_PLAIN_KEY_FUNC)
+
+#define SEC_SUBSCRIBER_KEY_FIELDS(x, y) x(FixedString<SUBSCRIBER_SUB_NBR>, SUB_NBR)
+#define SEC_SUBSCRIBER_VALUE_FIELDS(x, y) x(uint32_t, S_ID)
+
+#define SEC_SUBSCRIBER_GET_PLAIN_KEY_FUNC               \
+        uint64_t get_plain_key() const                  \
+        {                                               \
+                return std::stoi(SUB_NBR.c_str());      \
+        }
+
+DO_STRUCT(sec_subscriber, SEC_SUBSCRIBER_KEY_FIELDS, SEC_SUBSCRIBER_VALUE_FIELDS, NAMESPACE_FIELDS, SEC_SUBSCRIBER_GET_PLAIN_KEY_FUNC)
+
+#define ACCESS_INFO_DATA_3_SIZE 3
+#define ACCESS_INFO_DATA_4_SIZE 5
+
+#define ACCESS_INFO_KEY_FIELDS(x, y) x(uint32_t, S_ID) y(uint8_t, AI_TYPE)
+#define ACCESS_INFO_VALUE_FIELDS(x, y) x(uint8_t, DATA_1) y(uint8_t, DATA_2) y(FixedString<ACCESS_INFO_DATA_3_SIZE>, DATA_3) y(FixedString<ACCESS_INFO_DATA_4_SIZE>, DATA_4)
+
+#define ACCESS_INFO_GET_PLAIN_KEY_FUNC                  \
+        uint64_t get_plain_key() const                  \
+        {                                               \
+                return S_ID * UINT32_MAX + AI_TYPE;     \
+        }
+
+DO_STRUCT(access_info, ACCESS_INFO_KEY_FIELDS, ACCESS_INFO_VALUE_FIELDS, NAMESPACE_FIELDS, ACCESS_INFO_GET_PLAIN_KEY_FUNC)
 
 namespace star
 {
@@ -76,6 +102,41 @@ template <> class ClassOf<tatp::subscriber::value> {
 		return ClassOf<decltype(tatp::subscriber::value::SUB_NBR)>::size() + ClassOf<decltype(tatp::subscriber::value::BITS)>::size() +
                         ClassOf<decltype(tatp::subscriber::value::HEXES)>::size() + ClassOf<decltype(tatp::subscriber::value::BYTES)>::size() +
                         ClassOf<decltype(tatp::subscriber::value::MSC_LOCATION)>::size() + ClassOf<decltype(tatp::subscriber::value::VLR_LOCATION)>::size();
+	}
+};
+
+template <> class Serializer<tatp::access_info::value> {
+    public:
+	std::string operator()(const tatp::access_info::value &v)
+	{
+		return Serializer<decltype(v.DATA_1)>()(v.DATA_1) + Serializer<decltype(v.DATA_2)>()(v.DATA_2) +
+                        Serializer<decltype(v.DATA_3)>()(v.DATA_3) + Serializer<decltype(v.DATA_4)>()(v.DATA_4);
+        }
+};
+
+template <> class Deserializer<tatp::access_info::value> {
+    public:
+	std::size_t operator()(StringPiece str, tatp::access_info::value &result) const
+	{
+                std::size_t data_1_sz = Deserializer<decltype(result.DATA_1)>()(str, result.DATA_1);
+		str.remove_prefix(data_1_sz);
+                std::size_t data_2_sz = Deserializer<decltype(result.DATA_1)>()(str, result.DATA_1);
+		str.remove_prefix(data_2_sz);
+                std::size_t data_3_sz = Deserializer<decltype(result.DATA_1)>()(str, result.DATA_1);
+		str.remove_prefix(data_3_sz);
+                std::size_t data_4_sz = Deserializer<decltype(result.DATA_1)>()(str, result.DATA_1);
+		str.remove_prefix(data_4_sz);
+
+                return data_1_sz + data_2_sz + data_3_sz + data_4_sz;
+	}
+};
+
+template <> class ClassOf<tatp::access_info::value> {
+    public:
+	static constexpr std::size_t size()
+	{
+		return ClassOf<decltype(tatp::access_info::value::DATA_1)>::size() + ClassOf<decltype(tatp::access_info::value::DATA_2)>::size() +
+                        ClassOf<decltype(tatp::access_info::value::DATA_3)>::size() + ClassOf<decltype(tatp::access_info::value::DATA_4)>::size();
 	}
 };
 
