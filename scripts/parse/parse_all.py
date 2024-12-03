28b9d0e69d06d17aeb016c194c07bc58aeb4c3ec
ybhuang <ybhuang@cs.utexas.edu>
2024-12-03 21:32:33 +0000

    parse and plot new YCSB read-only figure

--- scripts/parse/parse_all.py
+++ scripts/parse/parse_all.py
@@ -89,6 +89,21 @@ def parse_ycsb(pasha_res_dir, baseline_res_dir, rw_ratio, zipf_theta):
         header_row = ["Remote_Ratio", "0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]
         parse_results(input_file_list, output_file_name, header_row)
 
+def construct_input_list_ycsb_with_read_cxl(pasha_res_dir, baseline_res_dir, rw_ratio, zipf_theta):
+        input_file_list = list()
+        input_file_list.append(("Tigon", pasha_res_dir + "/ycsb-TwoPLPasha-rmw-8-3-" + rw_ratio + "-" + zipf_theta + "-1-0-" + "Clock" + "-" + "OnDemand" + "-" + "209715200" + "-" + "WriteThrough" + "-" + "None" + "-" + "GROUP_WAL" + ".txt"))
+        input_file_list.append(("Tigon-ReadCXL", pasha_res_dir + "/ycsb-TwoPLPashaReadCXL-rmw-8-3-" + rw_ratio + "-" + zipf_theta + "-1-0-" + "Clock" + "-" + "OnDemand" + "-" + "209715200" + "-" + "WriteThrough" + "-" + "None" + "-" + "GROUP_WAL" + ".txt"))
+
+        input_file_list.append(("Sundial-CXL-improved", baseline_res_dir + "/ycsb-Sundial-rmw-8-3-" + rw_ratio + "-" + zipf_theta + "-1-0-" + "NoMoveOut" + "-" + "OnDemand" + "-" + "0" + "-" + "NoOP" + "-" + "None" + "-" + "GROUP_WAL" + ".txt"))
+        input_file_list.append(("TwoPL-CXL-improved", baseline_res_dir + "/ycsb-TwoPL-rmw-8-3-" + rw_ratio + "-" + zipf_theta + "-1-0-" + "NoMoveOut" + "-" + "OnDemand" + "-" + "0" + "-" + "NoOP" + "-" + "None" + "-" + "GROUP_WAL" + ".txt"))
+        return input_file_list
+
+def parse_ycsb_with_read_cxl(pasha_res_dir, baseline_res_dir, rw_ratio, zipf_theta):
+        input_file_list = construct_input_list_ycsb_with_read_cxl(pasha_res_dir, baseline_res_dir, rw_ratio, zipf_theta)
+        output_file_name = pasha_res_dir + "/ycsb-with-read-cxl-" + rw_ratio + "-" + zipf_theta + ".csv"
+        header_row = ["Remote_Ratio", "0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]
+        parse_results(input_file_list, output_file_name, header_row)
+
 def construct_input_list_tpcc(pasha_res_dir, baseline_res_dir):
         input_file_list = list()
         input_file_list.append(("Tigon", pasha_res_dir + "/tpcc-TwoPLPasha-8-3-1-0-" + "Clock" + "-" + "OnDemand" + "-" + "209715200" + "-" + "WriteThrough" + "-" + "None" + "-" + "GROUP_WAL" + ".txt"))
@@ -120,6 +135,7 @@ parse_ycsb_baseline(baseline_res_dir + "/macro", "50", "0.7")
 parse_tpcc_baseline(baseline_res_dir + "/macro")
 
 ### pasha and baselines ###
+parse_ycsb_with_read_cxl(pasha_res_dir + "/micro", baseline_res_dir + "/micro", "100", "0.7")
 parse_ycsb(pasha_res_dir + "/micro", baseline_res_dir + "/micro", "100", "0.7")
 parse_ycsb(pasha_res_dir + "/micro", baseline_res_dir + "/micro", "0", "0.7")
 parse_ycsb(pasha_res_dir + "/macro", baseline_res_dir + "/macro", "95", "0.7")
---
+++ scripts/plot/plot_ycsb_with_read_cxl.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+if len(sys.argv) != 4:
+        print("Usage: " + sys.argv[0] + " res_dir rw_ratio zipf_theta")
+        sys.exit(-1)
+
+res_dir = sys.argv[1]
+rw_ratio = sys.argv[2]
+zipf_theta = sys.argv[3]
+
+marker_size = 10.0
+marker_edge_width=1.6
+linewidth = 1.0
+
+###  Common configurations for TPCC and YCSB ###
+basic_font = {'family' : 'times new roman', 'size' : 12}
+plt.xticks(**basic_font)
+plt.yticks(**basic_font)
+
+# Configure grid
+plt.grid(axis='y')
+
+### plot TPCC ###
+res_csv = res_dir + "/ycsb-with-read-cxl-" + rw_ratio + "-" + zipf_theta + ".csv"
+
+# Read the CSV file into a Pandas DataFrame
+res_df = pd.read_csv(res_csv)
+
+# Extract the data
+x = res_df["Remote_Ratio"]
+
+tigon_y = res_df["Tigon"]
+tigon_read_cxl_y = res_df["Tigon-ReadCXL"]
+sundial_cxl_improved_y = res_df["Sundial-CXL-improved"]
+twopl_cxl_improved_y = res_df["TwoPL-CXL-improved"]
+
+plt.xlabel("Multi-partition Transaction Percentage", **basic_font)
+plt.ylabel("Throughput (txns/sec)", **basic_font)
+
+# Configure axis range
+tmp_list = list()
+tmp_list.extend(tigon_y)
+tmp_list.extend(tigon_read_cxl_y)
+tmp_list.extend(sundial_cxl_improved_y)
+tmp_list.extend(twopl_cxl_improved_y)
+
+max_y = max(tmp_list)
+max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
+plt.ylim(0, max_y_rounded_up)
+
+# Transform Y axises
+ax = plt.subplot(111)
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K' if x != 0 else 0))
+
+# Create the line plot
+plt.plot(x, tigon_y, color="#ffc003", marker="^", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Tigon")
+plt.plot(x, tigon_read_cxl_y, color="#CD5C5C", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Tigon-ReadCXL")
+
+plt.plot(x, sundial_cxl_improved_y, color="#BDB76B", marker="^", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Sundial-CXL-improved")
+plt.plot(x, twopl_cxl_improved_y, color="#8B008B", marker=">", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-CXL-improved")
+
+# Configure legend
+ax.legend(loc='upper center', frameon=False, fancybox=False, framealpha=1, ncol=2, prop={**basic_font})
+
+plt.savefig(res_dir + "/ycsb-with-read-cxl-" + rw_ratio + "-" + zipf_theta + ".pdf", format="pdf", bbox_inches="tight")
