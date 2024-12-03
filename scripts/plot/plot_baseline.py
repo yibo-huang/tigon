607d74ba264770af1bb397ef185083b2b2c46672
Newton Ni <nwtnni@gmail.com>
2024-12-03 16:12:16 -0600

    Plot YCSB baselines

--- scripts/plot/plot_baseline.py
+++ scripts/plot/plot_baseline.py
@@ -1,88 +1,128 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 import math
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-
-if len(sys.argv) != 2:
-    print("Usage: " + sys.argv[0] + " res_dir")
-    sys.exit(-1)
-
-res_dir = sys.argv[1]
 
 marker_size = 10.0
 marker_edge_width = 1.6
 linewidth = 1.0
 
-### plot TPCC ###
-res_csv = res_dir + "/baseline-tpcc.csv"
 
-# Read the CSV file into a Pandas DataFrame
-res_df = pd.read_csv(res_csv, index_col=0)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("res_dir")
+    parser.add_argument("-z", "--zipf-theta")
+    parser.add_argument("-r", "--rw-ratio")
+    args = parser.parse_args()
 
-# Extract the data
-xs = res_df.index
-
-# Configure axis range
-max_y = res_df.to_numpy().max()
-max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
-plt.ylim(0, max_y_rounded_up)
-
-figure, axes = plt.subplots(nrows=1, ncols=2, sharey=True)
-figure.set_size_inches(w=6, h=3)
-figure.set_layout_engine("constrained")
-figure.supxlabel("Multi-partition Transaction Percentage (NewOrder/Payment)")
-figure.supylabel("Throughput (txns/sec)")
-
-for (label, row), marker in zip(
-    res_df.T.loc[["Sundial-CXL-improved", "Sundial-CXL", "Sundial-NET"]].iterrows(),
-    ["^", "s", "o"],
-):
-    axes[0].plot(
-        xs,
-        row,
-        color="#FFC003",
-        marker=marker,
-        markersize=marker_size,
-        linewidth=linewidth,
-        markeredgewidth=marker_edge_width,
-        mfc="none",
-        label=label,
-    )
-
-for (label, row), marker in zip(
-    res_df.T.loc[["TwoPL-CXL-improved", "TwoPL-CXL", "TwoPL-NET"]].iterrows(),
-    ["^", "s", "o"],
-):
-    axes[1].plot(
-        xs,
-        row,
-        color="#4372C4",
-        marker=marker,
-        markersize=marker_size,
-        linewidth=linewidth,
-        markeredgewidth=marker_edge_width,
-        mfc="none",
-        label=label,
-    )
-
-for axis in axes:
-    axis.tick_params(axis="x", labelsize=8.0)
-    axis.grid(axis="y")
-    axis.yaxis.set_major_formatter(
-        ticker.FuncFormatter(
-            lambda x, pos: "{:,.0f}".format(x / 1000) + "K" if x != 0 else 0
+    csv = None
+    benchmark = None
+    if args.zipf_theta is not None and args.rw_ratio is not None:
+        benchmark = "ycsb"
+        csv = os.path.join(
+            args.res_dir,
+            "micro",
+            f"baseline-ycsb-{args.rw_ratio}-{args.zipf_theta}.csv",
         )
+    else:
+        benchmark = "tpcc"
+        csv = os.path.join(args.res_dir, "macro", "baseline-tpcc.csv")
+
+    df = pd.read_csv(csv, index_col=0)
+    xs = df.index
+
+    # Configure axis range
+    max_y = df.to_numpy().max()
+    max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
+    plt.ylim(0, max_y_rounded_up)
+
+    figure, axes = plt.subplots(nrows=1, ncols=2, sharey=True)
+    figure.set_size_inches(w=6, h=3)
+    figure.set_layout_engine("constrained")
+    figure.supxlabel(
+        "Multi-partition Transaction Percentage"
+        + (" (NewOrder/Payment)" if benchmark == "tpcc" else "")
     )
-    axis.legend(
-        loc="upper right",
-        frameon=False,
-        fancybox=False,
-        framealpha=1,
-        markerfirst=False,
-    )
+    figure.supylabel("Throughput (txns/sec)")
+
+    # Plot Sundial on left
+    for (label, row), marker in zip(
+        df.T.loc[["Sundial-CXL-improved", "Sundial-CXL", "Sundial-NET"]].iterrows(),
+        ["^", "s", "o"],
+    ):
+        axes[0].plot(
+            xs,
+            row,
+            color="#FFC003",
+            marker=marker,
+            markersize=marker_size,
+            linewidth=linewidth,
+            markeredgewidth=marker_edge_width,
+            mfc="none",
+            label=label,
+        )
+
+    # Plot TwoPL on right
+    for (label, row), marker in zip(
+        df.T.loc[["TwoPL-CXL-improved", "TwoPL-CXL", "TwoPL-NET"]].iterrows(),
+        ["^", "s", "o"],
+    ):
+        axes[1].plot(
+            xs,
+            row,
+            color="#4372C4",
+            marker=marker,
+            markersize=marker_size,
+            linewidth=linewidth,
+            markeredgewidth=marker_edge_width,
+            mfc="none",
+            label=label,
+        )
+
+    for axis in axes:
+        axis.tick_params(axis="x", labelsize=8.0)
+        axis.grid(axis="y")
+
+        formatter = None
+        if max_y > 1e6:
+            formatter = ticker.FuncFormatter(
+                lambda x, pos: 0 if x == 0 else f"{x / 1e6:.1f}M"
+            )
+        else:
+            formatter = ticker.FuncFormatter(
+                lambda x, pos: 0 if x == 0 else f"{int(x / 1e3)}K"
+            )
+
+        axis.yaxis.set_major_formatter(formatter)
+        axis.legend(
+            loc="upper right",
+            frameon=False,
+            fancybox=False,
+            framealpha=1,
+            markerfirst=False,
+        )
+
+    if benchmark == "tpcc":
+        plt.savefig(
+            os.path.join(args.res_dir, "macro", "baseline-tpcc.pdf"),
+            format="pdf",
+            bbox_inches="tight",
+        )
+    else:
+        plt.savefig(
+            os.path.join(
+                args.res_dir,
+                "micro",
+                f"baseline-ycsb-{args.rw_ratio}-{args.zipf_theta}.pdf",
+            ),
+            format="pdf",
+            bbox_inches="tight",
+        )
 
 
-plt.savefig(res_dir + "/baseline-tpcc.pdf", format="pdf", bbox_inches="tight")
+if __name__ == "__main__":
+    main()
