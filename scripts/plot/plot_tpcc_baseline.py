8f3c0992353141e8908d8b5dc7f6b21e2596c536
Yibo Huang <ybhuang@cs.utexas.edu>
2024-12-08 20:06:33 -0600

    baseline figure done

--- scripts/plot/plot_tpcc_baseline.py
+++ scripts/plot/plot_tpcc_baseline.py
@@ -3,8 +3,24 @@
 import sys
 import math
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+
+### common config START ###
+
+DEFAULT_PLOT = {
+    "markersize": 6.0,
+    "markeredgewidth": 1.2,
+    "markerfacecolor": "none",
+    "markevery": 1,
+    "linewidth": 1.0,
+}
+
+plt.rcParams["font.size"] = 11
+
+### common config END ###
+
 
 if len(sys.argv) != 2:
         print("Usage: " + sys.argv[0] + " res_dir")
@@ -12,23 +28,8 @@ if len(sys.argv) != 2:
 
 res_dir = sys.argv[1]
 
-marker_size = 10.0
-marker_edge_width=1.6
-linewidth = 1.0
-
-###  Common configurations for TPCC and YCSB ###
-basic_font = {'family' : 'times new roman', 'size' : 12}
-plt.xticks(**basic_font)
-plt.yticks(**basic_font)
-
-# Configure grid
-plt.grid(axis='y')
-
-### plot TPCC ###
-res_csv = res_dir + "/baseline-tpcc.csv"
-
 # Read the CSV file into a Pandas DataFrame
-res_df = pd.read_csv(res_csv)
+res_df = pd.read_csv(res_dir + "/baseline-tpcc.csv")
 
 # Extract the data
 x = res_df["Remote_Ratio"]
@@ -36,14 +37,14 @@ x = res_df["Remote_Ratio"]
 sundial_cxl_improved_y = res_df["Sundial-CXL-improved"]
 sundial_cxl_y = res_df["Sundial-CXL"]
 sundial_net_y = res_df["Sundial-NET"]
+
 twopl_cxl_improved_y = res_df["TwoPL-CXL-improved"]
 twopl_cxl_y = res_df["TwoPL-CXL"]
 twopl_net_y = res_df["TwoPL-NET"]
 
-plt.xlabel("Multi-partition Transaction Percentage", **basic_font)
-plt.ylabel("Throughput (txns/sec)", **basic_font)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 3.5))
 
-# Configure axis range
+### calculate Y limit for 1 and 2 ###
 tmp_list = list()
 tmp_list.extend(sundial_cxl_improved_y)
 tmp_list.extend(sundial_cxl_y)
@@ -54,23 +55,38 @@ tmp_list.extend(twopl_net_y)
 
 max_y = max(tmp_list)
 max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
-plt.ylim(0, max_y_rounded_up)
 
-# Transform Y axises
-ax = plt.subplot(111)
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K' if x != 0 else 0))
+### subplot 1 ###
+ax[0].set_ylim(0, max_y_rounded_up)
+ax[0].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K' if x != 0 else 0))
 
-# Create the line plot
-plt.plot(x, sundial_cxl_improved_y, color="#BDB76B", marker="^", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Sundial-CXL-improved")
-plt.plot(x, twopl_cxl_improved_y, color="#8B008B", marker=">", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-CXL-improved")
+ax[0].plot(x, sundial_cxl_improved_y, color="#4372c4", marker="s", **DEFAULT_PLOT, label="Sundial+")
+ax[0].plot(x, sundial_cxl_y, color="#4372c4", marker="^", **DEFAULT_PLOT, label="Sundial-CXL")
+ax[0].plot(x, sundial_net_y, color="#4372c4", marker="o", **DEFAULT_PLOT, label="Sundial-NET")
 
-plt.plot(x, sundial_cxl_y, color="#ffc003", marker="^", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Sundial-CXL")
-plt.plot(x, twopl_cxl_y, color="#62615d", marker=">", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-CXL")
+ax[0].legend(loc='upper right', frameon=True, fancybox=True, framealpha=1, ncol=1)
 
-plt.plot(x, sundial_net_y, color="#CD5C5C", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="Sundial-NET")
-plt.plot(x, twopl_net_y, color="#4372c4", marker="s", markersize=marker_size, linewidth=linewidth, markeredgewidth=marker_edge_width, mfc='none', label="TwoPL-NET")
+### subplot 2 ###
+ax[1].set_ylim(0, max_y_rounded_up)
+ax[1].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K' if x != 0 else 0))
 
-# Configure legend
-ax.legend(loc='upper center', frameon=False, fancybox=False, framealpha=1, ncol=2, prop={**basic_font})
+ax[1].plot(x, twopl_cxl_improved_y, color="#ffc003", marker="s", **DEFAULT_PLOT, label="DS2PL+")
+ax[1].plot(x, twopl_cxl_y, color="#ffc003", marker="^", **DEFAULT_PLOT, label="DS2PL-CXL")
+ax[1].plot(x, twopl_net_y, color="#ffc003", marker="o", **DEFAULT_PLOT, label="DS2PL-NET")
+
+ax[1].legend(loc='upper right', frameon=True, fancybox=True, framealpha=1, ncol=1)
+
+ax[1].set_yticklabels([])
+
+### global configuration ###
+for ax in ax.flat:
+    ax.grid(axis='y')
+
+fig = plt.gcf()
+fig.tight_layout()
+# fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.04), frameon=False, fancybox=False, framealpha=1, ncol=4)
+
+fig.text(0.5, 0, 'Multi-partition Transaction Percentage', ha='center')
+fig.text(0, 0.5, 'Throughput (txns/sec)', va='center', rotation='vertical')
 
 plt.savefig(res_dir + "/baseline-tpcc.pdf", format="pdf", bbox_inches="tight")
