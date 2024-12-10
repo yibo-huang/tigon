823354c1566e6cb37f9128105e976edb7dcc33e7
Yibo Huang <ybhuang@cs.utexas.edu>
2024-12-09 22:11:15 -0600

    new TPCC figures

--- scripts/plot/plot_tpcc_twopl.py
+++ scripts/plot/plot_tpcc_twopl.py
@@ -25,32 +25,35 @@ plt.rcParams["font.size"] = 14
 plt.grid(axis='y')
 
 ### plot TPCC ###
-res_csv = res_dir + "/tpcc.csv"
+res_csv = res_dir + "/baseline-tpcc.csv"
 
 # Read the CSV file into a Pandas DataFrame
 res_df = pd.read_csv(res_csv)
 
-# Extract the data
 x = res_df["Remote_Ratio"]
 
-tigon_y = res_df["Tigon"]
 sundial_cxl_improved_y = res_df["Sundial-CXL-improved"]
+sundial_cxl_y = res_df["Sundial-CXL"]
+sundial_net_y = res_df["Sundial-NET"]
+
 twopl_cxl_improved_y = res_df["TwoPL-CXL-improved"]
-motor_y = res_df["Motor"]
+twopl_cxl_y = res_df["TwoPL-CXL"]
+twopl_net_y = res_df["TwoPL-NET"]
 
 plt.xlabel("Multi-partition Transaction Percentage")
 plt.ylabel("Throughput (txns/sec)")
 
-# Configure axis range
+### calculate Y limit for 1 and 2 ###
 tmp_list = list()
-tmp_list.extend(tigon_y)
 tmp_list.extend(sundial_cxl_improved_y)
+tmp_list.extend(sundial_cxl_y)
+tmp_list.extend(sundial_net_y)
 tmp_list.extend(twopl_cxl_improved_y)
-tmp_list.extend(motor_y)
+tmp_list.extend(twopl_cxl_y)
+tmp_list.extend(twopl_net_y)
 
 max_y = max(tmp_list)
 max_y_rounded_up = math.ceil(max_y / 200000.0) * 200000.0
-plt.ylim(0, max_y_rounded_up)
 
 # Transform Y axises
 ax = plt.subplot(111)
@@ -58,12 +61,12 @@ plt.ylim(0, 600000)
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 'K' if x != 0 else 0))
 
 # Create the line plot
-plt.plot(x, tigon_y, color="#000000", marker="s", **DEFAULT_PLOT, label="Tigon")
-plt.plot(x, sundial_cxl_improved_y, color="#4372c4", marker="^", **DEFAULT_PLOT, markerfacecolor="none", label="Sundial+")
-plt.plot(x, twopl_cxl_improved_y, color="#ffc003", marker=">", **DEFAULT_PLOT, markerfacecolor="none", label="DS2PL+")
-plt.plot(x, motor_y, color="#ed7d31", marker="o", **DEFAULT_PLOT, markerfacecolor="none", label="Motor")
+plt.plot(x, twopl_cxl_improved_y, color="#ffc003", marker="s", **DEFAULT_PLOT, markerfacecolor="none", label="DS2PL+")
+plt.plot(x, twopl_cxl_y, color="#ffc003", marker="^", **DEFAULT_PLOT, markerfacecolor="none", label="DS2PL-CXL")
+plt.plot(x, twopl_net_y, color="#ffc003", marker=">", **DEFAULT_PLOT, markerfacecolor="none",label="DS2PL-NET")
 
 # Configure legend
 ax.legend(loc='upper right', frameon=True, fancybox=True, framealpha=1, ncol=1)
 
-plt.savefig(res_dir + "/tpcc.pdf", format="pdf", bbox_inches="tight")
+plt.savefig(res_dir + "/tpcc-twopl.pdf", format="pdf", bbox_inches="tight")
+
